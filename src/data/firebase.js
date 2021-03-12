7d7b0dbf7890d61f874720d2e6d918210c35e6f6
Lauryan <lauryan-gervais@hotmail.com>
2021-03-12 16:53:02 -0500

    config de base firebase

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -5,14 +5,7 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDYXixKoQkDQRBQM6g_v0lspnYwWsUV2OM",
-  authDomain: "veille-technologique-173fb.firebaseapp.com",
-  databaseURL: "https://veille-technologique-173fb-default-rtdb.firebaseio.com",
-  projectId: "veille-technologique-173fb",
-  storageBucket: "veille-technologique-173fb.appspot.com",
-  messagingSenderId: "884338243671",
-  appId: "1:884338243671:web:01f43ca82deb29efa98b12",
-  measurementId: "G-706FZ2JG5N"
+
 };
 
 // Initialiser Firebase
